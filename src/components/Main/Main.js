425ff93330af14d090b66503d5ac032915ae2e18
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-11-07 21:45:21 +0300

    "feat: add Techs"

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import Header from '../Header/Header'
 import Promo from '../Promo/Promo'
 import AboutProject from '../AboutProject/AboutProject'
-// import Techs from '../Techs/Techs'
+import Techs from '../Techs/Techs'
 // import AboutMe from '../AboutMe/AboutMe'
 // import Portfolio from '../Portfolio/Portfolio'
 // import Footer from '../Footer/Footer'
@@ -12,7 +12,8 @@ function Main() {
 			<Header />
       <Promo />
       <AboutProject />
-      {/* <Techs />
+      <Techs />
+      {/* 
       <AboutMe />
       <Portfolio />
 			<Footer /> */}
