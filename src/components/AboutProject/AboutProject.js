7058a39a35bd914376c76e6a773700657fa2d833
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-11-07 15:56:03 +0300

    "feat: add AboutProject"

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -5,18 +5,18 @@ function AboutProject() {
       <section className="about-project" id="about-project">
         <h2 className="about-project__title">О проекте</h2>
         <div className="about-project__container">
-          <h3 className="about-project__description-title">Дипломный проект включал 5 этапов</h3>
+          <h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
           <p className="about-project__description">Составление плана, работу над бэкендом, вёрстку,
           добавление функциональности и финальные доработки.</p>
-          <h3 className="about-project__description-title">На выполнение диплома ушло 5 недель</h3>
+          <h3 className="about-project__subtitle">На выполнение диплома ушло 5 недель</h3>
           <p className="about-project__description">У каждого этапа был мягкий и жёсткий дедлайн, которые
           нужно было соблюдать, чтобы успешно защититься.</p>
         </div>
         <div className="about-project__duration">
-          <p className="about-project__duration-diagram about-project__duration-diagram_backend">1 неделя</p>
-          <p className="about-project__duration-diagram about-project__duration-diagram_frontend">4 недели</p>
-          <p className="about-project__duration-stage-name">Back-end</p>
-          <p className="about-project__duration-stage-name">Front-end</p>
+          <p className="about-project__duration-line about-project__duration-line_backend">1 неделя</p>
+          <p className="about-project__duration-line about-project__duration-line_frontend">4 недели</p>
+          <p className="about-project__duration-text">Back-end</p>
+          <p className="about-project__duration-text">Front-end</p>
         </div>
       </section>
     )
